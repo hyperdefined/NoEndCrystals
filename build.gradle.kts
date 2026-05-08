76ecf58bb7298a47bfe278c88f8655fd8d342f51
trard <trardone@gmail.com>
2026-05-08 16:38:50 +0300

    old api

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     compileOnly("com.github.hyperdefined:hyperlib:1.0.14:all")
 }
 
